d1ed3e062ae3b517eda9e66e59c944914e8e0e5d
RobertValdimarsson <robert.valdimarsson@gmail.com>
2015-03-18 01:04:44 +0100

    bla bla bla

---
+++ Ebene1/Kit Design.mm
@@ -0,0 +1,416 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1426069051772" ID="ID_793083378" MODIFIED="1426090988780" TEXT="Kit Design">
+<edge STYLE="linear" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1426069164568" ID="ID_1685374832" MODIFIED="1426105491369" POSITION="right" TEXT="PCB Design">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426069185078" ID="ID_1899075475" MODIFIED="1426090988718" TEXT="Preis nur akzeptabel bei hohen Auflagen (100+)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426069198029" ID="ID_86115193" MODIFIED="1426090988720">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Intelligentes PCB Design kann Geh&#228;use(kosten) sparen
+    </p>
+    <p>
+      (z.B. &#181;Amp, ATX-PSU)
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426069496762" ID="ID_1034222494" MODIFIED="1426090988725">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      KiCad kostenlos, aber nur wenige Hersteller
+    </p>
+    <p>
+      Andere EDA ben&#246;tigen Lizenz (Eagle)
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070500110" ID="ID_74544315" MODIFIED="1426090988729" TEXT="Testpunkte f&#xfc;r Lerneffekt">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071399170" ID="ID_1094473247" MODIFIED="1426090988730" TEXT="BON/Testroutine entwerfen">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071788030" ID="ID_1330281932" MODIFIED="1426090988730" TEXT="Keine chinesischen Hersteller">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072561188" ID="ID_1951292161" MODIFIED="1426090988731" TEXT="evtl. SMD vormontieren">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072664147" ID="ID_948481244" MODIFIED="1426090988731" TEXT="Farbe/Form (wiedererkennungswert)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426069647416" ID="ID_245480969" MODIFIED="1426090988733" POSITION="left" TEXT="Verpackung und Versand">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426069660368" ID="ID_766799953" MODIFIED="1426090988733" TEXT="Verpackung ESD safe">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426069680960" ID="ID_404259132" MODIFIED="1426090988733">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Briefsendungen (auch in Luftpolstertaschen) gef&#228;hrlich
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070116402" ID="ID_961552407" MODIFIED="1426090988736" TEXT="Lithiumbatterien und Gefahrstoffe">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071209789" ID="ID_956548570" MODIFIED="1426090988736" TEXT="Bei Versand ins Ausland: Zoll, Normen, Restriktionen">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071441154" ID="ID_1631664406" MODIFIED="1426090988737" TEXT="Widerruf, Retouren etc. kl&#xe4;ren (wann Userfehler, wann Kitfehler?)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426069759671" ID="ID_1459417001" MODIFIED="1426090988738" POSITION="right" TEXT="Geh&#xe4;use">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426069765879" ID="ID_1513503244" MODIFIED="1426090988739">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Geh&#228;use treibt Preis in die H&#246;he besser&#160;als Zubeh&#246;r
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426069796182" ID="ID_1498761635" MODIFIED="1426090988742">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      PCB Design nach Geh&#228;use&#160;ausrichten
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426069837694" ID="ID_1884987665" MODIFIED="1426090988744" TEXT="dauerhafte Verf&#xfc;gbarkeit">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070644916" ID="ID_653370081" MODIFIED="1426090988745" TEXT="Doppelacrylscheibengeh&#xe4;use (Lasercut)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072633979" ID="ID_730450091" MODIFIED="1426090988745" TEXT="Beschriftung/&#xc4;tzung/Costum vermeiden (Preis)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426069877157" ID="ID_1390942589" MODIFIED="1426090988747" POSITION="left" TEXT="Bauelemente">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426069895349" ID="ID_1123472311" MODIFIED="1426090988747" TEXT="akzeptabler Preis nur bei gro&#xdf;en Mengen">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426069923285" ID="ID_1121619381" MODIFIED="1426090988747" STYLE="fork" TEXT="dauerhafte Verf&#xfc;gbarkeit oder einfache Ersetzbarkeit">
+<edge COLOR="#808080" STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426069943540" ID="ID_506415778" MODIFIED="1426090988748" TEXT="Herstellersicherheit (&#x2260;Reichelt)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070059955" ID="ID_248148141" MODIFIED="1426090988748" TEXT="digikey, farnell, mouser (aber Porto, Zoll)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070172978" ID="ID_1178755657" MODIFIED="1426090988748" TEXT="BOM durch Design verkleinern (zB R Werte anpassen)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072543212" ID="ID_1034566323" MODIFIED="1426090988749" TEXT="SMD vermeiden">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426073352202" ID="ID_767813818" MODIFIED="1426090988749" STYLE="fork" TEXT="Off-the-shelf-parts wo m&#xf6;glich">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426070081187" ID="ID_193738398" MODIFIED="1426090988750" POSITION="right" TEXT="Sicherheit">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426070089051" ID="ID_1096898942" MODIFIED="1426090988750" TEXT="Erdpotential vermeiden (USB etc)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070225289" ID="ID_1876846646" MODIFIED="1426090988751" TEXT="Netzspannung vermeiden">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070238801" ID="ID_1820193492" MODIFIED="1426090988751" TEXT="R&#xf6;hren etc. vermeiden">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070964032" ID="ID_276742158" MODIFIED="1426090988751" TEXT="Design pr&#xfc;fen lassen (von jmdm der Ahnung hat)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071241916" ID="ID_584290609" MODIFIED="1426090988752" TEXT="N&#xf6;tige Zertifikate vermeiden (EMI, CE, BNA (Funk), etc.)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071276492" ID="ID_1710800542" MODIFIED="1426090988752" TEXT="N&#xf6;tige Normen einhalten (Schnittstellen, Signale, Adapter)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426070132874" ID="ID_1482680335" MODIFIED="1426090988753" POSITION="left" TEXT="Preisgestaltung">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426070147170" ID="ID_55335922" MODIFIED="1426090988753" TEXT="Schmerzgrenzen (20, 40&#x20ac;)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070270648" ID="ID_142272289" MODIFIED="1426090988753" TEXT="Gewinnmarge beachten">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070283232" ID="ID_480205076" MODIFIED="1426090988754">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Faktoren:
+    </p>
+    <p>
+      -Arbeitszeit (Design, Vorl&#246;ten, Verpackung)
+    </p>
+    <p>
+      -PCB Herstellung (SMD PaP)
+    </p>
+    <p>
+      -Bauelemente
+    </p>
+    <p>
+      -Dokumentation (Druck und Erstellung)
+    </p>
+    <p>
+      -Geh&#228;use (Laser Cutting, Standard)
+    </p>
+    <p>
+      -Verpackungsmaterial + Versand
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070794090" ID="ID_1811744767" MODIFIED="1426090988761">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Anbieten von PCB einzeln, Kit, Geh&#228;use,
+    </p>
+    <p>
+      Erweiterungen (zB Kabel, Adapter, Netzteil) etc
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072832001" ID="ID_1702460294" MODIFIED="1426151597790">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Kosten einer Charge m&#252;ssen vorgeschossen werden
+    </p>
+    <p>
+      (Kickstarter/Vorbestellungen?)
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426070423847" ID="ID_1640242180" MODIFIED="1426090988766" POSITION="right" TEXT="Dokumentation">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426070436286" ID="ID_1222737045" MODIFIED="1426090988766" TEXT="Funktion, Aufbau, Schaltungsprinzip">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070451310" ID="ID_997847916" MODIFIED="1426090988766" TEXT="Bauanleitung">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1426070528173" ID="ID_1057878917" MODIFIED="1426090988767" TEXT="Erkl&#xe4;rung der Bauelemente + Kennzeichnung">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1426070546581" ID="ID_1130855355" MODIFIED="1426090988767" TEXT="Schritt-f&#xfc;r-Schritt-Anleitung">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1426070561909" ID="ID_1996490501" MODIFIED="1426090988767" TEXT="N&#xf6;tiges Werkzeug">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1426070458254" ID="ID_1540360903" MODIFIED="1426090988768" TEXT="Warnhinweise">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1426071530873" ID="ID_1396844361" MODIFIED="1426090988768" TEXT="Sicherheitshinweise">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1426071540529" ID="ID_276041008" MODIFIED="1426090988768" TEXT="ESD Hinweise (Garantie)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1426070485982" ID="ID_1661256330" MODIFIED="1426090988769" TEXT="Experimente, Messungen an Testpunkten">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071141294" ID="ID_1473588437" MODIFIED="1426090988769" TEXT="Impressum/Kontakt">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071347819" ID="ID_90007327" MODIFIED="1426090988769" TEXT="Print on Demand oder in Kit Auflage drucken">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071596672" ID="ID_1700629696" MODIFIED="1426090988770" TEXT="Welchen Kenntnisstand voraussetzen (Zielgruppe)?">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426070581397" ID="ID_167037359" MODIFIED="1426090988771" POSITION="left" TEXT="Werbung, Pr&#xe4;sentation">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426070589116" ID="ID_959207107" MODIFIED="1426090988771" TEXT="Pr&#xe4;sentation auf Website">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070601524" ID="ID_1494900068" MODIFIED="1426090988771" TEXT="Gute Fotos">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070608020" ID="ID_1969734070" MODIFIED="1426090988772" TEXT="Rezensionen auf Blogs (MES, EEV etc)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071152581" ID="ID_465325495" MODIFIED="1426090988772" TEXT="Kontakt per Webseite, schnelle Beantwortung von Anfragen">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426071647351" ID="ID_465712104" MODIFIED="1426090988772" TEXT="Unterscheidung von n&#xf6;tigen Vorkenntnissen bei Kits">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426070839033" ID="ID_1666313515" MODIFIED="1426090988773" POSITION="right" TEXT="Lizenz">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426070845593" ID="ID_881951559" MODIFIED="1426090988773">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Copyright und Prior Art beachten (evtl. Nachfragen/Namensnennung)
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426070894265" ID="ID_1163852005" MODIFIED="1426090988776" TEXT="OSHW, CC oder GPL (=&gt; Design &#xf6;ffentlich machen)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426072197657" ID="ID_918548202" MODIFIED="1426090988777" POSITION="left" TEXT="Kits">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426072206088" ID="ID_768035267" MODIFIED="1426090988777" TEXT="Audio/Synth/SigGen (APC, NE555Synth, XR2209)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072224392" ID="ID_1797264500" MODIFIED="1426090988778" TEXT="Allgemeine Elektronik (BatW&#xe4;chter, Thermometer etc)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072309463" ID="ID_213561341" MODIFIED="1426090988778" TEXT="Wearables (Schl&#xfc;sselanh&#xe4;nger, Armb&#xe4;nder etc)">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426072375342" ID="ID_15527679" MODIFIED="1426090988779" POSITION="right" TEXT="Unternehmensrechtliches">
+<edge STYLE="linear"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1426072396918" ID="ID_793792441" MODIFIED="1426090988779" TEXT="Rechnungsstellung">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426072388574" ID="ID_905522160" MODIFIED="1426090988780" TEXT="Steuer">
+<edge STYLE="linear" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#00b439" CREATED="1426091000236" ID="ID_1111581640" MODIFIED="1426091006175" TEXT="Anmeldung">
+<edge STYLE="linear" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
